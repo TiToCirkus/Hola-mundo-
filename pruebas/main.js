832c3205d081c87b9892990cf791c576f7a3d497
Jose A Alvarez <joseantonioalvarez821@gmail.com>
2020-12-08 22:52:45 -0500

    nueva prueba

--- pruebas/main.js
+++ pruebas/main.js
@@ -1,8 +1,30 @@
-const button = document.getElementById('btn');
 
-function changefoot(){
-    document.getElementById('footer').style.fontSize = 'x-large'
-    document.getElementById('footer__background').style.backgroundColor = 'blue'
+class householdAppliance {
+    constructor(price,color,energyConsumption,capacity){
+        this.price = price
+        this.color = color
+        this.energyConsumption = energyConsumption
+        this.capacity = capacity
+    }
+
+    getcapacity(){
+        return this.capacity
+    }
+
+    setEnergyConsumption(newEnergy){
+        this.energyConsumption = newEnergy;
+
+        return this.energyConsumption
+    }
+
 }
 
-button.addEventListener('click', changefoot)
+const fridge = new householdAppliance('200$','white','95%','4000L')
+const freezer = new householdAppliance('450$','grey','50%','7000L')
+const oven = new householdAppliance('350$','black','25%','50L')
+const blender = new householdAppliance('50$','orange','100%','3L')
+const mixer = new householdAppliance('60$','brow','100%','0L')
+
+fridge.setEnergyConsumption('52L')
+console.log(fridge)
+
